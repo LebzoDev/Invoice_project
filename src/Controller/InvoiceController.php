d1615623c58b3e6cdbcb75eff21476d5cb3140e8
lebzodev <leboundiayee123@gmail.com>
2022-04-10 07:27:12 +0000

    add edit update and delete methode to invoice entity

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -56,13 +56,63 @@ class InvoiceController extends AbstractController
 
 
      /**
-     * @Route("/lit", name="list")
+     * @Route("/list", name="list")
      */
     public function list()
     {
+        $repository = $this->getDoctrine()->getRepository(Invoice::class);
+        $invoices = $repository->findAll();
+
+        // dd($invoices);
+
         return $this->render('invoice/list.html.twig', [
             'controller_name' => 'InvoiceController',
+            'invoices' => $invoices,
         ]);
+    }
+
+
+    /**
+     * @Route("/edit/{id}", name="invoice_edit")
+     */
+    public function edit(Invoice $invoice, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(InvoiceFormType::class, $invoice);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $lines = $invoice->getInvoiceLines();
+            foreach ($lines as $line) {
+                $line->setTotalWithVat((string)($line->getQuantity() * (int) $line->getAmount() + ((int) $line->getVatAmount())));
+                $manager->persist($line);
+                $line->setInvoice($invoice);
+            }
+        
+            $manager->persist($invoice);
+            $manager->flush();
+
+            
+            unset($invoice);
+            unset($form);
+
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('invoice/index.html.twig', [
+            'controller_name' => 'InvoiceController',
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/delete/{id}", name="invoice_delete")
+     */
+    public function delete(Invoice $invoice, EntityManagerInterface $manager)
+    {
+        $manager->remove($invoice);
+        $manager->flush();
+
+        return $this->redirectToRoute('home');
     }
     
 }
